5832863c47c9df6a64ecba5d5f8881d480575aad
JQiue <861947542@qq.com>
2021-05-19 23:52:07 +0800

    feat: updates some notes and changes

--- docs/.vuepress/config/sideBar/language.js
+++ docs/.vuepress/config/sideBar/language.js
@@ -3,25 +3,25 @@ module.exports = [
   {
     title: 'C',
     prefix: '/language/c/',
-    collapsable: false,
-    children: ['1', '2', '3', '4', '5', '6']
+    collapsable: true,
+    children: ['1', '2', '3', '4', '5', '6', '7']
   },
   {
     title: 'Java',
     prefix: '/language/java/',
-    collapsable: false,
+    collapsable: true,
     children: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
   },
   {
     title: 'JavaScript',
     prefix: '/language/js/',
-    collapsable: false,
+    collapsable: true,
     children: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
   },
   {
     title: 'Python',
     prefix: '/language/python/',
-    collapsable: false,
+    collapsable: true,
     children: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
   }
 ];
